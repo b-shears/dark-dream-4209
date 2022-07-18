929a52e103a4cb18d2adacb207d482790fe7af29
Bryan Shears <brshears@gmail.com>
2022-07-17 21:34:02 -0600

    Add feat/routes/view/model for recipes index

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe 'recipe index page'  do
 
   it 'displays a list of recipes with attributes' do
-    spaghetti = Recipe.create!(Name: "Spaghetti", Complexity: 2, Genre: "Italian")
-    possum_chili = Recipe.create!(Name: "Possum Chili", Complexity: 3, Genre: "Cajun")
-    deep_fried_twinkie = Recipe.create!(Name: "Deep Fried Twinks", Complexity: 4, Genre: "Dessert")
+    spaghetti = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
+    possum_chili = Recipe.create!(name: "Possum Chili", complexity: 3, genre: "Cajun")
+    deep_fried_twinkie = Recipe.create!(name: "Deep Fried Twinks", complexity: 4, genre: "Dessert")
 
     visit '/recipes'
 
     within "#recipes-#{spaghetti.id}" do
       expect(page).to have_content("Name: Spaghetti")
       expect(page).to have_content("Complexity: 2")
-      expect(page).to have_content("Genre: Cajun")
+      expect(page).to have_content("Genre: Italian")
     end
 
     within "#recipes-#{possum_chili.id}" do
-      expect(page).to have_content("Name: Spaghetti")
-      expect(page).to have_content("Complexity: 2")
-      expect(page).to have_content("Genre: Italian")
+      expect(page).to have_content("Name: Possum Chili")
+      expect(page).to have_content("Complexity: 3")
+      expect(page).to have_content("Genre: Cajun")
     end
 
     within "#recipes-#{deep_fried_twinkie.id}" do
